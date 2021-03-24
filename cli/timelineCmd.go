c04f692dcd143d920c082ae7eb1003f68a23be02
マリウス <marius@xn--gckvb8fzb.com>
2021-03-23 22:35:57 -0500

    Enhanced timelineCmd

--- cli/timelineCmd.go
+++ cli/timelineCmd.go
@@ -4,9 +4,14 @@ import (
   "fmt"
   "log"
   "context"
+  "image/color"
 
   "github.com/spf13/cobra"
-  // "github.com/mattn/go-mastodon"
+
+  "github.com/grokify/html-strip-tags-go"
+  "html"
+
+  "github.com/eliukblau/pixterm/pkg/ansimage"
 )
 
 var timelineCmd = &cobra.Command{
@@ -19,7 +24,19 @@ var timelineCmd = &cobra.Command{
       log.Fatal(err)
     }
     for i := len(timeline) - 1; i >= 0; i-- {
-      fmt.Println(timeline[i])
+      fmt.Printf("%s [%s]\n", timeline[i].Account.DisplayName, timeline[i].Account.Acct)
+      fmt.Println(html.UnescapeString(strip.StripTags(timeline[i].Content)))
+      for _, attachment := range timeline[i].MediaAttachments {
+        pix, err := ansimage.NewScaledFromURL(attachment.PreviewURL, 30, 40, color.Transparent, ansimage.ScaleModeResize, ansimage.NoDithering)
+        if err != nil {
+          fmt.Println(err)
+        }
+        if err == nil {
+          fmt.Printf("%s\n", pix.Render())
+        }
+      }
+
+      fmt.Printf("\n")
     }
 
     return
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/mrusme/gomphotherium
 go 1.15
 
 require (
+	github.com/eliukblau/pixterm v1.3.1
+	github.com/gizak/termui/v3 v3.1.0
+	github.com/grokify/html-strip-tags-go v0.0.1
 	github.com/mattn/go-mastodon v0.0.5-0.20200727014106-315df7d9162e
 	github.com/spf13/cobra v1.1.3
 )
