2fc3070bc2a9c54729559fb22437526e57177ff5
マリウス <marius@xn--gckvb8fzb.com>
2021-04-03 00:05:31 -0500

    Cleaned up code

--- tui/timeline.go
+++ tui/timeline.go
@@ -20,28 +20,39 @@ const (
 )
 
 func Timeline(MastodonClient *mastodon.Client) {
-    app := tview.NewApplication()
+  app := tview.NewApplication()
 
-    logoBox := tview.NewTextView().
+  input := tview.NewInputField().
+    SetLabel("@user.instance.org: ").
+    SetLabelColor(tcell.ColorDefault).
+    SetFieldBackgroundColor(tcell.ColorDefault).
+    SetDoneFunc(func(key tcell.Key) {
+      // app.Stop()
+    })
+
+  stream := tview.NewTextView().
     SetDynamicColors(true).
     SetRegions(true).
     SetWrap(true)
 
-    box := tview.NewFlex().
-    SetDirection(tview.FlexRow).
-    AddItem(logoBox, 0, 1, true)
+  grid := tview.NewGrid().
+    SetRows(0, 1).
+    SetColumns(0).
+    SetBorders(true).
+    AddItem(stream, 0, 0, 1, 1, 0, 0, false).
+    AddItem(input, 1, 0, 1, 1, 0, 0, true)
 
-    app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-      if event.Key() == tcell.KeyCtrlN {
-        _, _, w, _ := logoBox.Box.GetInnerRect()
-        output := mast.Timeline(MastodonClient, w)
-        fmt.Fprint(logoBox, tview.TranslateANSI(output))
-        return nil
-      }
-      return event
-    })
-
-    if err := app.SetRoot(box, true).Run(); err != nil {
-      panic(err)
+  app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+    if event.Key() == tcell.KeyCtrlN {
+      _, _, w, _ := stream.Box.GetInnerRect()
+      output := mast.Timeline(MastodonClient, w)
+      fmt.Fprint(stream, tview.TranslateANSI(output))
+      return nil
     }
+    return event
+  })
+
+  if err := app.SetRoot(grid, true).Run(); err != nil {
+    panic(err)
+  }
 }
